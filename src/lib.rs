4319644e1ce5c53226620213371f633e2468d73d
Mirlan Ipasov <mipasov@gmail.com>
2023-05-28 21:28:01 +0600

    functions use clone of the vector and makes operations

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,27 @@
-use::std::io::Result;
+
 
 pub fn print_menu() {
     println!("-1 to exit!\n1.add\n2.list\n3.delete\n4.edit\n5.find\n");
 }
 
-pub fn add<'a>(contact: &'a str, addr_book: Vec<&'a str>) -> Vec<&'a str>{
-  [&addr_book[..], &[&contact]].concat()
+pub fn print_menu_add() {
+  println!("Enter contact in format of - Name Surname Phonenumber : ");
+}
+
+pub fn add<T: Clone>(contact: T, addr_book: &Vec<T>) -> Vec<T>{  
+  let mut list_internal = addr_book.to_vec();
+  list_internal.push(contact);
+  list_internal
 }
 
 pub fn delete<'a>(number: &'a str, addr_book: Vec<&'a str>) -> Vec<&'a str> {
   addr_book.into_iter().filter(|c| !c.contains(number)).collect::<Vec<&str>>()
+}
+
+pub fn list<T: Clone + std::fmt::Display>(addr_book: &Vec<T>) {
+  for contact in addr_book {
+    print!("{}", contact);
+  }
 }
 
 #[cfg(test)]
@@ -19,14 +31,14 @@ mod test {
   fn add_on_empty() {
     let contact = "John Smith 1234567890";
     let addr_book = vec![];
-    let new_addr_book = add(contact, addr_book);
+    let new_addr_book = add(contact, &addr_book);
     assert_eq!(1, new_addr_book.len());
   }
   #[test]
   fn add_on_non_empty() {
     let contact = "John Smith 1234567890";
     let addr_book = vec![contact];
-    let new_addr_book = add(contact, addr_book);
+    let new_addr_book = add(contact, &addr_book);
     assert_eq!(2, new_addr_book.len());
   }
 
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,35 @@
-use std::io::{self, BufRead};
+use std::{io::{self}};
 
-fn main() {}
+use addressbook::{print_menu, print_menu_add, add, list};
+ 
+fn main() {
+    let mut address_list = Vec::new();
+    loop {
+        print_menu();
+        let mut buffer = String::new();
+        io::stdin().read_line(&mut buffer).unwrap();
+        print!("{}", buffer);
+        match buffer.as_str().trim() {
+             "1" => {
+                print_menu_add();
+                let contact = read_string_from_stdin();
+                if !contact.is_empty() {
+                    let new_addr_list = add(contact, &address_list);
+                    new_addr_list.clone_into(&mut address_list);
+                    
+                    list(&address_list);
+                }
+            },
+            "-1" => break,
+            _ => println!("not yet implemented")
+        }
+    }
     
+    
+}
+    
+fn read_string_from_stdin() -> String{
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer);
+    buffer.as_str().trim().to_owned()
+}
